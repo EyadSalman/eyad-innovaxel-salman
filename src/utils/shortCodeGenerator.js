a1d14132abbea129c2a7b942f086e269189025dc
EyadSalman <l217695@lhr.nu.edu.pk>
2025-07-17 21:26:45 +0500

    Add short code generator util

---
+++ src/utils/shortCodeGenerator.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto');
+
+module.exports = function generateCode(length = 6) {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
